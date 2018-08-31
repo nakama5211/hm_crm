1259a24d9a059f1afc35cb55c50910a6aaa64af5
Nguyen Quang Sang <sangnguyen@Nguyens-MacBook-Pro-2.local>
2018-08-31 15:08:13 +0700

    Sang

--- application/views/user/user_create.php
+++ application/views/user/user_create.php
@@ -7,9 +7,6 @@
         <div class="col-md-3 no-padding padding-left-15">
           	<?php echo isset($left) ? $left : ''; ?>
         </div>
-        <div class="col-md-9">
-          	<?php echo isset($center) ? $center : ''; ?>
-        </div>
         <div class="col-md-2 col-md-22 no-padding padding-left-5 padding-top-10 padding-right-10">
         	<?php echo isset($right) ? $right : ''; ?>
         </div>
